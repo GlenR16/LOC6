8d7023dfeb81e939b8e3751a1b7f01b6eedeb40e
Glenn Mendonca <glenem17@gmail.com>
2024-03-09 13:27:10 +0530

    Frontend Routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { HomeLayout } from "../layouts/home.layout";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeLayout />}>
+          <Route index element={<>Landing</>} />
+          <Route
+            path="/auth"
+            element={
+              <>
+                Auth <Outlet />
+              </>
+            }
+          >
+            <Route path="login" element={<>Login</>} />
+            <Route path="register" element={<>Register</>} />
+          </Route>
+          <Route path="dashboard" element={<>Dash board</>} />
+          <Route path="profile" element={<>Profile</>} />
+          <Route  path="/"/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
